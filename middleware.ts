7c1178bb485342a4d32ad52b8df9ec11a22c9754
Harris Grant <harris@Harriss-MacBook-Pro.local>
2026-02-01 20:16:57 +0000

    update cron middleware

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ export { default } from "next-auth/middleware";
 export const config = {
   matcher: [
     // Protect everything except Next internals, auth routes, and the login page
-    "/((?!api/auth|login|_next|favicon.ico|.*\\.(?:png|jpg|jpeg|svg|webp|gif)).*)",
+    "/((?!api/auth|api/cron|login|_next|favicon.ico|.*\\.(?:png|jpg|jpeg|svg|webp|gif)).*)",
   ],
 };
 
